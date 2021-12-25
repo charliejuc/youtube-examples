bb4a28d6895140607fc680030c4d5b34178b1f93
Juan Carlos <charliejuc@gmail.com>
2021-12-25 12:16:51 +0100

    Adds thunk example

---
+++ thunk.js
@@ -0,0 +1,77 @@
+const db = {
+  users: [
+    {
+      id: 1,
+      username: 'charlie',
+    },
+    {
+      id: 2,
+      username: 'otto',
+    },
+    {
+      id: 3,
+      username: 'su',
+    },
+  ],
+};
+
+const getUser = (userId) =>
+  new Promise((resolve) =>
+    setTimeout(
+      (userId) => {
+        const user = db.users.find((user) => user.id === userId);
+
+        resolve(user);
+      },
+      200,
+      userId
+    )
+  );
+
+const functionAsyncCache = (fn) => {
+  const cache = {};
+
+  return async (...args) => {
+    const key = JSON.stringify(args);
+
+    if (key in cache) {
+      return cache[key];
+    }
+
+    cache[key] = await fn(...args);
+
+    return cache[key];
+  };
+};
+const thunk =
+  (fn) =>
+  (...args) =>
+  () =>
+    fn(...args);
+
+const getCharlie = functionAsyncCache(thunk(getUser)(1));
+const getOtto = functionAsyncCache(thunk(getUser)(2));
+
+const isCharlie = async (lazyUser) => {
+  const user = await lazyUser();
+
+  return user.username === 'charlie';
+};
+
+(async () => {
+  console.time('getCharlie');
+  console.log(await getCharlie());
+  console.timeEnd('getCharlie');
+
+  console.log();
+
+  console.time('isCharlie');
+  console.log(await isCharlie(getCharlie));
+  console.timeEnd('isCharlie');
+
+  console.log();
+
+  console.time('isNotCharlie');
+  console.log(await isCharlie(getOtto));
+  console.timeEnd('isNotCharlie');
+})();
